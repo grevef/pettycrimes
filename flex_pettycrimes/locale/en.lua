170ef3b2d4a074e101b1191aa3b0a9b143b3137d
Haddeus <utviklermail@gmail.com>
2025-12-06 09:13:14 +0100

    Added English & Norwegian Locales.
    Added English & Norwegian Locales.

--- flex_pettycrimes/locale/en.lua
+++ flex_pettycrimes/locale/en.lua
@@ -1,12 +1,32 @@
 if Config.Lang == 'en' then
     Language = {
         success = {
+            blackouthack = 'Electrical box disabled',
+            placesecondrod = 'Now place the same piece on the opposite side to tighten the rope and activate the trap',
         },
         error = {
+            tofar = 'You are too far..',
+            maxtraps = 'You cannot place any more right now..',
+            nothingfound = 'Nothing found..',
         },
         progress = {
+            grabbing = 'Stealing..',
+            placing = 'Placing..',
+            taking = 'Taking..',
+            stealtyre = 'Stealing tire..',
+            digging = 'Digging up..',
+            searchgrave = 'Searching coffin..',
+            takedeadstuff = 'Checking pockets..',
         },
         target = {
+            grabbag = 'Steal bag..',
+            stealbox = 'Pick up box..',
+            blackout = 'Hacking..',
+            stealtyre = 'Steal tire..',
+            removerop = 'Remove rope..',
+            diggrave = 'Dig..',
+            searchgrave = 'Search..',
+            takedeadstuff = 'Steal..',
         },
         info = {
         },
@@ -16,5 +36,11 @@ if Config.Lang == 'en' then
         },
         radial = {
         },
+        dispatch = {
+            smahgrab = 'Suspicious person smashing a window..',
+            postrob = 'Suspicious person stealing a package..',
+            stealtyre = 'Suspicious person removing a tire..',
+            GraveRob = 'Suspicious person digging..',
+        }
     }
 end
---
+++ flex_pettycrimes/locale/no.lua
@@ -0,0 +1,46 @@
+if Config.Lang == 'en' then
+    Language = {
+        success = {
+            blackouthack = 'Strømboksen er deaktivert',
+            placesecondrod = 'Plasser nå den samme delen på motsatt side for å stramme tauet og aktivere fellen',
+        },
+        error = {
+            tofar = 'Du er for langt unna..',
+            maxtraps = 'Du kan ikke plassere flere nå..',
+            nothingfound = 'Ingenting funnet..',
+        },
+        progress = {
+            grabbing = 'Stjeler..',
+            placing = 'Plasserer..',
+            taking = 'Tar..',
+            stealtyre = 'Stjeler dekk..',
+            digging = 'Graver opp..',
+            searchgrave = 'Søker i kiste..',
+            takedeadstuff = 'Sjekker lommer..',
+        },
+        target = {
+            grabbag = 'Stjel sekk..',
+            stealbox = 'Plukk opp eske..',
+            blackout = 'Hacker..',
+            stealtyre = 'Stjel dekk..',
+            removerop = 'Fjern tau..',
+            diggrave = 'Grav..',
+            searchgrave = 'Søk..',
+            takedeadstuff = 'Stjel..',
+        },
+        info = {
+        },
+        command = {
+        },
+        menu = {
+        },
+        radial = {
+        },
+        dispatch = {
+            smahgrab = 'Mistenkelig person knuser et vindu..',
+            postrob = 'Mistenkelig person stjeler en pakke..',
+            stealtyre = 'Mistenkelig person løsner et dekk..',
+            GraveRob = 'Mistenkelig person graver på stedet..',
+        }
+    }
+end
